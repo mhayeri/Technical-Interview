c3539c96d61b1965b7a0da630d2c8d1924ec572f
mhayeri <hayerimustafa@gmail.com>
2023-07-07 17:08:44 -0700

    LeetCode Problem #1207 - Unique Number of Occurrences

---
+++ LeetCode/Java/Easy/UniqueNumberOfOccurrences.java
@@ -0,0 +1,24 @@
+/**
+ * 1207. Unique Number of Occurrences
+ * Difficulty: Easy
+ * Description: Given an array of integers arr, return true if the number of occurrences of each value
+ * in the array is unique or false otherwise.
+ *
+ * Input: arr = [1,2,2,1,1,3]
+ * Output: true
+ * Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+ */
+public class UniqueNumberOfOccurrences {
+    Map<Integer, Integer> counts = new HashMap<>();
+        for (int num : arr) {
+        counts.put(num, counts.getOrDefault(num, 0) + 1);
+    }
+
+    Set<Integer> frequencies = new HashSet<>();
+        for (Integer n : counts.keySet()) {
+        if (frequencies.contains(counts.get(n))) return false;
+        frequencies.add(counts.get(n));
+    }
+
+        return true;
+}
