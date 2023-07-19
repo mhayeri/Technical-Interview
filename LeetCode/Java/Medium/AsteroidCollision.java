8f03ffe01dc1a291a92013cfaba340f4bf69e652
mhayeri <hayerimustafa@gmail.com>
2023-07-18 17:36:34 -0700

    LeetCode Problem #735 - Asteroid Collision

---
+++ LeetCode/Java/Medium/AsteroidCollision.java
@@ -0,0 +1,49 @@
+/**
+ * 735. Asteroid Collision
+ * Difficulty: Medium
+ * Description: We are given an array asteroids of integers representing asteroids in a row.
+ *
+ * For each asteroid, the absolute value represents its size, and the sign represents its direction
+ * (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+ *
+ * Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode.
+ * If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+ *
+ * Input: asteroids = [5,10,-5]
+ * Output: [5,10]
+ * Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
+ *
+ * Input: asteroids = [8,-8]
+ * Output: []
+ * Explanation: The 8 and -8 collide exploding each other.
+ *
+ * Input: asteroids = [10,2,-5]
+ * Output: [10]
+ * Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+ */
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for (int asteroid : asteroids) {
+            boolean shouldAdd = true;
+            while (!stack.isEmpty() && (stack.peek() > 0 && asteroid < 0)) {
+                if (Math.abs(stack.peek()) < Math.abs(asteroid)) {
+                    stack.pop();
+                    continue;
+                } else if (Math.abs(stack.peek()) == Math.abs(asteroid)) {
+                    stack.pop();
+                }
+                shouldAdd = false;
+                break;
+            }
+            if (shouldAdd) stack.push(asteroid);
+        }
+
+        int[] res = new int[stack.size()];
+        for (int i = res.length - 1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+
+        return res;
+    }
+}
