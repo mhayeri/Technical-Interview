a5613ac18ffe28604227197075ef17596e7ece8f
mhayeri <hayerimustafa@gmail.com>
2024-06-09 12:50:53 -0700

    Modified LeetCode Problem #143 - Reorder List

--- LeetCode/Python/Medium/reorder_list.py
+++ LeetCode/Python/Medium/reorder_list.py
@@ -1,25 +1,21 @@
 #
-#   735. Asteroid Collision
+#   143. Reorder List
 #   Difficulty: Medium
-#   Description: We are given an array asteroids of integers representing asteroids in a row. For each asteroid, the
-#   absolute value represents its size, and the sign represents its direction (positive meaning right, negative
-#   meaning left). Each asteroid moves at the same speed.
+#   Description: You are given the head of a singly linked-list. The list can be represented as:
 #
-#   Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If
-#   both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+#   L0 → L1 → … → Ln - 1 → Ln
 #
+#   Reorder the list to be on the following form:
+#   L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 #
-#   Input: asteroids = [5,10,-5]
-#   Output: [5,10]
-#   Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never collide.
-
-#   Input: asteroids = [8,-8]
-#   Output: []
-#   Explanation: The 8 and -8 collide exploding each other.
+#   You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 #
-#   Input: asteroids = [10,2,-5]
-#   Output: [10]
-#   Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide resulting in 10.
+#   Input: head = [1,2,3,4]
+#   Output: [1,4,2,3]
+#
+#   Input: head = [1,2,3,4,5]
+#   Output: [1,5,2,4,3]
+#
 #
 
 from Shared.list_node import ListNode
